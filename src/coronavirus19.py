f046d55d09448ec39db32daa24d99bb9a2da2524
Ramon Maria Gallart <rgallart@ramagaes.com>
2020-04-05 17:02:05 -0700

    Main point of entrance.
    Now the script is an app. It has a main point of entrance where you pass the data and can give a config. To execute it, the command is:
    python coronavirus19.py <path to data file> [--config <path to config file>]
    Where:
      - <path to data file> is a mandatory argument. It is the path to your data.
      - --config <path to config file> is optional. If given, the config file is a JSON file that defines all configuration options. If not given, it will use the default configuration.
    
    You can also run:
    
    python coronavirus19.py --help
    
    To see the help menu.
    
    Enjoy

--- src/coronavirus19.py
+++ src/coronavirus19.py
@@ -4,44 +4,61 @@
 #                                 24/03/2020
 ################################################################################
 
-import os
-import sys
+import argparse
 
-import numpy as np
 import matplotlib.pyplot as plt
 
 from initialconditions import Config
 from models import CombinationAndProbabilityModel
 
-# IMPORTING CONSTANTS FROM initialconditions module
-# Refer to the module for a description of each value
-config = Config()
 
-# Creating the model
-model = CombinationAndProbabilityModel(config, os.path.join('..', 'real_data', 'Spain_Corona.txt'))
+def plot(config, model):
+    plt.ylim(0, config.plot_y_lim)
+    plt.xlim(0, config.plot_x_lim)
 
-model.run_model()
+    if config.plot_infected:
+        plt.plot(range(0, config.days), model.infected, label='infected')
 
-plt.ylim(0, config.plot_y_lim)
-plt.xlim(0, config.plot_x_lim)
+    if config.plot_immune:
+        plt.plot(range(0, config.days), model.immune, label='immune')
 
-if config.plot_infected:
-    plt.plot(range(0, config.days), model.infected, label='infected')
+    if config.plot_healthy:
+        plt.plot(range(0, config.days), model.healthy, label='healthy')
 
-if config.plot_immune:
-    plt.plot(range(0, config.days), model.immune, label='immune')
+    if config.plot_deaths:
+        plt.plot(range(0, config.days), model.deaths, label='Deaths')
 
-if config.plot_healthy:
-    plt.plot(range(0, config.days), model.healthy, label='healthy')
+    if config.plot_total_cases:
+        plt.plot(range(0, config.days), model.total_cases, label='Total cases')
 
-if config.plot_deaths:
-    plt.plot(range(0, config.days), model.deaths, label='Deaths')
+    if config.plot_total_cases_experimental:
+        plt.plot(range(0, config.days), model.experimental, label='Total cases_experimental')
 
-if config.plot_total_cases:
-    plt.plot(range(0, config.days), model.total_cases, label='Total cases')
+    plt.legend()
+    plt.show()
 
-if config.plot_total_cases_experimental:
-    plt.plot(range(0, config.days), model.experimental, label='Total cases_experimental')
 
-plt.legend()
-plt.show()
+def run(data, configuration):
+    # IMPORTING CONSTANTS FROM initialconditions module
+    # Refer to the module for a description of each value
+    config = Config()
+    if configuration:
+        with open(configuration) as f:
+            config.load(f)
+
+    # Creating the model
+    model = CombinationAndProbabilityModel(config, data)
+
+    model.run_model()
+
+    plot(config, model)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run some COVID-19 simulations")
+    parser.add_argument("data", help="Data to process")
+    parser.add_argument("--config", help="path to the configuration file")
+
+    args = parser.parse_args()
+
+    run(args.data, args.config)
